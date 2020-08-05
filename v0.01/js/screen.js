23dd56712ebebcc68485be5bc6e63b54d72afa0b
Lu Yu <yuluyolo0619@gmail.com>
2020-08-05 13:53:05 +1200

    update

--- v0.01/js/screen.js
+++ v0.01/js/screen.js
@@ -1,10 +1,10 @@
 function opensubpage(url){
-    document.getElementById('content_page').className='content_page_on';
+    document.getElementById('div_iframe').className='div_iframe_on';
     document.getElementById('close').className='close_on';
     document.getElementById('content_page').src='pages/'+url;
 }
 function closesubpage(){
-	document.getElementById('content_page').className='content_page_off';
+    document.getElementById('div_iframe').className='div_iframe_off';
 	document.getElementById('close').className='close_off';		
 	document.getElementById('content_page').src='/pages/';
 }
